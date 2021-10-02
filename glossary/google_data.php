e12256df97b3ebc7a3b91fba0d44a5a89d1e74f4
JayawardanaJTH <tiran2323@gmail.com>
2021-10-03 02:57:33 +0530

    gert google client id and secret

---
+++ glossary/google_data.php
@@ -0,0 +1,9 @@
+<?php
+
+$googleClientID = '28797251985-km773agd6qbh0c81rvg9ci0hhdf3nouo.apps.googleusercontent.com';
+$googleClientSecret = '7bP45_B-WDUw2UwOIaUNyKE4';
+
+$authorizeURL = 'https://accounts.google.com/o/oauth2/v2/auth';
+$tokenURL = 'https://oauth2.googleapis.com/token';
+$baseURL = 'https://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
+?>
