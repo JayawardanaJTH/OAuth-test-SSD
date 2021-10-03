ec9494537b25cc02113f77af477e331269483435
Viranga Dilshan <61415991+hvdilshan@users.noreply.github.com>
2021-10-04 03:11:10 +0530

    change fb login page

--- glossary/server/facebook_login.php
+++ glossary/server/facebook_login.php
@@ -4,11 +4,15 @@
 session_start();
 
 //include autoload file from vendor folder
-require '../../../vendor/autoload.php';
+require '../../vendor/autoload.php';
 
 $fb = new Facebook\Facebook([
     'app_id' => '612492623086513',
     'app_secret' => '13a71c4e33d467edcb6c354148541ab3',
     'default_graph_version' => 'v2.7'
 ]);
+
+$helper = $fb->getRedirectLoginHelper();
+$login_url = $helper->getLoginUrl("http://localhost/OAuth-test-SSD/glossary/client/php/");
+
 ?>
