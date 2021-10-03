eb5881a85f0b88722eaff75d330a73f9c5205a4b
JayawardanaJTH <tiran2323@gmail.com>
2021-10-03 15:32:54 +0530

    Change header

---
+++ glossary/client/php/helpers/header.php
@@ -0,0 +1,22 @@
+<?php
+include('../../google_data.php');
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php echo $page_title ?></title>
+    <link rel="shortcut icon" type="image/jpg" href="../images/tick_icon.svg.png" />
+
+    <!-- Bootstrap Urls -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../styles/index.css">
+</head>
--- glossary/client/php/register.php
+++ glossary/client/php/register.php
@@ -1,23 +1,9 @@
 <?php
 include('../../google_data.php');
-session_start();
+$page_title = 'Store';
+include('helpers/header.php');
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Store</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-
-    <link rel="shortcut icon" type="image/jpg" href="../images/tick_icon.svg.png" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
-    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-
-    <link rel="stylesheet" href="../styles/index.css">
-</head>
 
 <body style="background-image: url(../images/back1.jpg); background-size: 1480px;">
     <!-- Header and navigation -->
