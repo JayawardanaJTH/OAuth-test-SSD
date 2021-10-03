57f65311409dfbf033f380a18a679de757c47b43
Viranga Dilshan <61415991+hvdilshan@users.noreply.github.com>
2021-10-04 03:00:13 +0530

    create facebook login file

---
+++ glossary/server/facebook_login.php
@@ -0,0 +1,14 @@
+<?php
+
+//start the session
+session_start();
+
+//include autoload file from vendor folder
+require '../../../vendor/autoload.php';
+
+$fb = new Facebook\Facebook([
+    'app_id' => '612492623086513',
+    'app_secret' => '13a71c4e33d467edcb6c354148541ab3',
+    'default_graph_version' => 'v2.7'
+]);
+?>
