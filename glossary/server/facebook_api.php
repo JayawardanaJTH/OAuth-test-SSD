fbf55211114c697dd3fbdf4d6487a0c30f3b1122
Viranga Dilshan <61415991+hvdilshan@users.noreply.github.com>
2021-10-04 20:45:14 +0530

    fb api done

---
+++ glossary/server/facebook_api.php
@@ -0,0 +1,34 @@
+<?php
+require '../../vendor/autoload.php';
+
+function get_fb_client()
+{
+    $fb = new Facebook\Facebook([
+        'app_id' => '4015107955262066',
+        'app_secret' => '0689381e103f8e70444885fa3de6cfad',
+        'graph_api_version' => 'v5.1.4'
+    ]);
+    return $fb;
+}
+
+function get_auth_url()
+{
+    $fb = get_fb_client();
+
+    $helper = $fb->getRedirectLoginHelper();
+
+    $auth_url = $helper->getLoginUrl('http://localhost:3000/glossary/server/facebook_login.php');
+
+    return $auth_url . 'public_profile%20user_posts%20user_friends%20user_photos';
+}
+
+function make_fb_token($data)
+{
+    $tokenPath = 'fb_token.json';
+    if (!file_exists(dirname($tokenPath))) {
+        mkdir(dirname($tokenPath), 0700, true);
+    }
+    file_put_contents($tokenPath, json_encode($data));
+
+}
+?>
